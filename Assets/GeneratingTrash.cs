3e20246960fc5841ef062662b69a98c455000f6e
YaroslavGozak <gozakyaroslav@gmail.com>
2019-10-05 06:46:20 +0300

    Added basic movement

---
+++ Assets/GeneratingTrash.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratingTrash : MonoBehaviour
+{
+    public GameObject myPrefab;
+    // Start is called before the first frame update
+    private List<GameObject> _trashItems;
+    private int _lastFrameCount;
+    private float _speed = 0.05F;
+    private System.Random _random;
+    void Start()
+    {
+        _trashItems = new List<GameObject>();
+        _lastFrameCount = 0;
+        _random = new System.Random();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPosition = GameObject.Find("Setelite").transform.transform.position;
+
+        foreach (var item in _trashItems)
+        {
+            item.transform.position = new Vector3(item.transform.position.x + _speed, item.transform.position.y, item.transform.position.z);
+        }
+
+        if (Time.frameCount - _lastFrameCount > _random.Next(80, 100))
+        {
+            var yShift = _random.Next(-20, 20);
+
+            var trash = Instantiate(myPrefab, new Vector3(playerPosition.x - 20, playerPosition.y + yShift, playerPosition.z), Quaternion.identity);
+
+            trash.AddComponent<CapsuleCollider>();
+
+            trash.transform.localScale = new Vector3(0.5F, 0.5F, 0.5F);
+            trash.tag = "trash";
+            _trashItems.Add(trash);
+
+            _lastFrameCount = Time.frameCount;
+        }
+
+        _trashItems.RemoveAll(i => i.transform.position.x - playerPosition.x > 30);
+    }
+}
---
+++ Assets/SeteliteController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeteliteController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var position = GetComponent<Renderer>().transform.position;
+        var rotation = GetComponent<Renderer>().transform.rotation;
+        var setelite = GameObject.Find("Setelite");
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Renderer>().transform.position = new Vector3(position.x, position.y + 0.1F, position.z);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Renderer>().transform.position = new Vector3(position.x, position.y - 0.1F, position.z);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            var angles = rotation.eulerAngles;
+            angles.z += 2;
+            Debug.Log("Rotating setelite + 2");
+            setelite.transform.Rotate(angles, 2);
+            Debug.Log(string.Format("Rotation: x {0}, y {1}, z {2}", setelite.transform.rotation.x, setelite.transform.rotation.y, setelite.transform.rotation.z));// + = new Quaternion(rotation.x, rotation.y, rotation.z + 1, rotation.w);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            var angles = rotation.eulerAngles;
+            angles.z += 2;
+            Debug.Log("Rotating setelite - 2");
+            setelite.transform.Rotate(angles, -2);
+            Debug.Log(string.Format("Rotation: x {0}, y {1}, z {2}", setelite.transform.rotation.x, setelite.transform.rotation.y, setelite.transform.rotation.z));//GetComponent<Renderer>().transform.rotation = new Quaternion(rotation.x, rotation.y, rotation.z - 1, rotation.w);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collapsed");
+        if(collision.gameObject.tag == "trash")
+        {
+            Application.Quit();
+        }
+    }
+}
