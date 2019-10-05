90aa116366b50dabb16ea2ca155e5e6ef2cd2fe7
imEmty <tony.mt.moro@gmail.com>
2019-10-05 13:23:23 +0300

    Total rework

---
+++ Assets/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Rigidbody Trash;
+    Vector3 angularVelocity;
+    public int rotationMultipier;
+
+    private void Start()
+    {
+        angularVelocity = rotationMultipier * Random.insideUnitSphere;
+        Trash = GetComponent<Rigidbody>();
+
+    }
+    void FixedUpdate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(angularVelocity * Time.deltaTime);
+        Trash.MoveRotation(Trash.rotation * deltaRotation);
+        
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float thrustForce;
+    void FixedUpdate()
+    {
+        
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, thrustForce * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, -thrustForce * Time.deltaTime, 0);
+        }
+    }
+}
