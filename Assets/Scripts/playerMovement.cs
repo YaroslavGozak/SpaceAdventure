9580d311e1b994f6bd2bc0a57c912f69422ad091
YaroslavGozak <gozakyaroslav@gmail.com>
2019-10-05 14:44:22 +0300

    Made trash spawner

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,16 +5,20 @@ public class playerMovement : MonoBehaviour
 
     public Rigidbody rb;
     public float thrustForce;
+
+    private float deltaPosition = 5F;
     void FixedUpdate()
     {
         
         if (Input.GetKey("w"))
         {
-            rb.AddForce(0, thrustForce * Time.deltaTime, 0);
+            var newPosition = new Vector3(rb.position.x, rb.position.y + deltaPosition * Time.deltaTime);
+            rb.MovePosition(newPosition);
         }
         if (Input.GetKey("s"))
         {
-            rb.AddForce(0, -thrustForce * Time.deltaTime, 0);
+            var newPosition = new Vector3(rb.position.x, rb.position.y - deltaPosition * Time.deltaTime);
+            rb.MovePosition(newPosition);
         }
     }
 }
