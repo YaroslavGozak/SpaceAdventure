c47c31c96931bbad94bf52436b22ceb5c5ae995a
VladislavKoba <ko1997vlad@gmail.com>
2019-10-07 21:26:36 +0300

    final fixes 2

--- Assets/GameStart.cs
+++ Assets/GameStart.cs
@@ -8,4 +8,5 @@ public class GameStart : MonoBehaviour
     {
         SceneManager.LoadScene("mainScene");
     }
+
 }
---
+++ Assets/readyManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class readyManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //SceneManager.LoadScene("mainScene");
+    }
+
+    void ReadyScene()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+}
